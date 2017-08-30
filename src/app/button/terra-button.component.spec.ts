d018f34d6831b9373bd37a1268d9bac2fe5f4b30
Michael Frank <michael.frank@plentymarkets.com>
2017-08-30 15:28:04 +0200

    - extends testing

--- src/app/button/terra-button.component.spec.ts
+++ src/app/button/terra-button.component.spec.ts
@@ -1,53 +1,60 @@
 /* tslint:disable:no-unused-variable */
 import { DebugElement } from '@angular/core';
 import {
-    TestBed,
     async,
-    ComponentFixture
+    ComponentFixture,
+    TestBed
 } from '@angular/core/testing';
 import { TerraButtonComponent } from './terra-button.component';
-import { TooltipModule } from 'ng2-bootstrap';
 import { By } from '@angular/platform-browser';
+import { TooltipModule } from 'ngx-bootstrap';
+
+describe('Component: TerraButtonComponent',
+         () =>
+         {
+             let comp:TerraButtonComponent;
+             let fixture:ComponentFixture<TerraButtonComponent>;
+             let de:DebugElement;
+             let el:HTMLElement;
+
+             beforeEach(async(() =>
+                              {
+                                  TestBed.configureTestingModule({
+                                                                     declarations: [TerraButtonComponent],
+                                                                     imports:      [TooltipModule.forRoot()]
+                                                                 })
+                                         .compileComponents()
+                                         .then(() =>
+                                               {
+                                                   fixture = TestBed.createComponent(TerraButtonComponent);
+                                                   comp = fixture.componentInstance;
+                                                   de = fixture.debugElement.query(By.css('.btn'));
+                                                   el = de.nativeElement;
+                                               });
+                              }));
 
 
-describe('Component: TerraButtonComponent', () =>
-{
-    let comp:TerraButtonComponent;
-    let fixture:ComponentFixture<TerraButtonComponent>;
-    let de:DebugElement;
-    let el:HTMLElement;
-    
-    
-    beforeEach(async(() =>
-                     {
-                         TestBed.configureTestingModule({
-                                                            declarations: [TerraButtonComponent],
-                                                            imports:      [TooltipModule.forRoot()]
-                                                        })
-                                .compileComponents()
-                                .then(() =>
-                                      {
-                                          fixture = TestBed.createComponent(TerraButtonComponent);
-                                          comp = fixture.componentInstance;
-                                          de = fixture.debugElement.query(By.css('.btn'));
-                                          el = de.nativeElement;
-                                      });
-                     }));
-    
-    
-    it('should create an instance', () =>
-    {
-        expect(comp).toBeTruthy();
-    });
-    
-    it('should emit an output event', () =>
-    {
-        spyOn(comp, 'click');
-        
-        el.click();
-        
-        fixture.whenStable().then(()=>{
-            expect(comp.click).toHaveBeenCalled();
-        });
-    });
-});
+             it('should create an instance',
+                () =>
+                {
+                    expect(comp).toBeTruthy();
+                });
+
+             it('should emit an output event',
+                () =>
+                {
+                    spyOn(comp,
+                          'outputClicked');
+
+                    el.click();
+
+                    fixture.whenStable()
+                           .then(() =>
+                                 {
+                                     comp.outputClicked.subscribe((output) =>
+                                                                  {
+                                                                      expect(output).toBeNull()
+                                                                  });
+                                 });
+                });
+         });
--- src/app/filter/terra-filter.component.spec.ts
+++ src/app/filter/terra-filter.component.spec.ts
@@ -1,11 +1,69 @@
-// /* tslint:disable:no-unused-variable */
-//
-// import { TestBed, async } from '@angular/core/testing';
-// import { PlentyFilterComponent } from './plenty-filter.component';
-//
-// describe('Component: TerraFilterComponent', () => {
-//   it('should create an instance', () => {
-//     let component = new PlentyFilterComponent();
-//     expect(component).toBeTruthy();
-//   });
-// });
+import {
+    async,
+    ComponentFixture,
+    TestBed
+} from '@angular/core/testing';
+import { TerraFilterComponent } from './terra-filter.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { TooltipModule } from 'ngx-bootstrap';
+import { TerraButtonComponent } from '../button/terra-button.component';
+import { TerraBaseToolbarComponent } from '../toolbar/base-toolbar/terra-base-toolbar.component';
+
+describe('Component: TerraFilterComponent',
+         () =>
+         {
+             let comp:TerraFilterComponent;
+             let fixture:ComponentFixture<TerraFilterComponent>;
+             let deSearchButton:DebugElement;
+             let deResetButton:DebugElement;
+
+             beforeEach(async(() =>
+                              {
+                                  TestBed.configureTestingModule({
+                                                                     declarations: [TerraFilterComponent,
+                                                                                    TerraButtonComponent,
+                                                                                    TerraBaseToolbarComponent],
+                                                                     imports:      [TooltipModule.forRoot()]
+                                                                 })
+                                         .compileComponents()
+                                         .then(() =>
+                                               {
+                                                   fixture = TestBed.createComponent(TerraFilterComponent);
+                                                   comp = fixture.componentInstance;
+                                                   deSearchButton = fixture.debugElement.queryAll(By.css('.terra-button'))[0];
+                                                   deResetButton = fixture.debugElement.queryAll(By.css('.terra-button'))[1];
+                                               })
+                              }));
+
+             it('should create an instance',
+                () =>
+                {
+                    expect(comp).toBeTruthy();
+                });
+
+             it('should have two buttons',
+                () =>
+                {
+                    expect(deSearchButton).toBeTruthy();
+                    expect(deResetButton).toBeTruthy();
+                });
+
+             it('should emit a search button clicked event',
+                () =>
+                {
+                    //deSearchButton.triggerEventHandler('snöff',
+                    //                                   null);
+
+                    fixture.whenStable()
+                           .then(() =>
+                                 {
+                                     comp.outputOnSearchBtnClicked.subscribe((output) =>
+                                                                             {
+                                                                                 console.log(output);
+                                                                                 expect(output).toBeNull();
+                                                                             });
+                                 });
+                });
+
+         });

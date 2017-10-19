e261fb5830f80abbdd304b89b2bbf0b68b39ab3a
Max Hellige <max.hellige@plentymarkets.com>
2017-10-19 14:01:53 +0200

    add toolbar example

--- src/app/toolbar/example/terra-base-toolbar.component.example.ts
+++ src/app/toolbar/example/terra-base-toolbar.component.example.ts
@@ -3,7 +3,7 @@ import {
 } from '@angular/core';
 
 @Component({
-    selector: 'terra-toolbar-example',
+    selector: 'terra-base-toolbar-example',
     styles:   [require('./terra-base-toolbar.component.example.scss')],
     template: require('./terra-base-toolbar.component.example.html')
 })

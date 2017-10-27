759480c9c545a376c1ebf077e05c1818706221cb
Maximilian Röll <maximilian.roell@plentymarkets.com>
2017-10-27 15:51:31 +0200

    added: example for input main component

---
+++ src/app/forms/input/example/terra-input.component.example.ts
@@ -0,0 +1,12 @@
+import {
+    Component
+} from "@angular/core";;
+
+@Component({
+               selector:  'terra-input-example',
+               styles:    [require('./terra-input.component.example.scss')],
+               template:  require('./terra-input.component.example.html'),
+           })
+export class TerraDoubleInputComponentExample
+{
+}

33d5a19c7702003fc74242dcc9c654074131f32d
Maximilian Röll <maximilian.roell@plentymarkets.com>
2017-11-24 13:26:02 +0100

    added: example for Terra-Suggestion-Box

--- src/app/forms/suggestion-box/example/terra-suggestion-box.component.example.ts
+++ src/app/forms/suggestion-box/example/terra-suggestion-box.component.example.ts
@@ -1,12 +1,44 @@
 import {
-    Component
+    Component,
+    OnInit
 } from "@angular/core";
+import { TerraSuggestionBoxValueInterface } from '../data/terra-suggestion-box.interface';
 
 @Component({
     selector: 'terra-suggestion-box-example',
     styles:   [require('./terra-suggestion-box.component.example.scss')],
     template: require('./terra-suggestion-box.component.example.html'),
 })
-export class TerraSuggestionBoxComponentExample
+export class TerraSuggestionBoxComponentExample implements OnInit
 {
+    private _iconList:Array<TerraSuggestionBoxValueInterface> = [];
+    private _iconClass: string;
+    ngOnInit()
+    {
+        this._iconClass = "icon-plugin";
+        this._iconList.push
+        (
+            {
+                value: "icon-plugin",
+                caption: "icon-plugin"
+            },
+            {
+                value: "icon-delete",
+                caption: "icon-delete"
+            },
+            {
+                value:"icon-add",
+                caption: "icon-add"
+            },
+            {
+                value:"icon-device_tablet",
+                caption: "icon-device_tablet"
+            },
+            {
+                value: "icon-box_plus",
+                caption: "icon-box_plus"
+            }
+        );
+    }
+
 }

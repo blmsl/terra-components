8f368736dea5c3108dc9d6b8c14a7d945016dbd7
Maximilian Röll <maximilian.roell@plentymarkets.com>
2017-11-27 16:56:23 +0100

    removed: descriptions

--- src/app/note/example/terra-note.component.example.md
+++
@@ -1,2 +0,0 @@
-#### Information 
-* The `terra-note` component is used to create and show notes.
--- src/app/note/terra-note.component.ts
+++ src/app/note/terra-note.component.ts
@@ -4,19 +4,15 @@ import {
 } from '@angular/core';
 
 @Component({
-    selector: 'terra-note',
-    styles:   [require('./terra-note.component.scss')],
-    template: require('./terra-note.component.html')
-})
+               selector: 'terra-note',
+               styles:   [require('./terra-note.component.scss')],
+               template: require('./terra-note.component.html')
+           })
 export class TerraNoteComponent
 {
-    /** @description If true, the note gets highlighted in blue.*/
     @Input() inputIsSelected:boolean;
-    /** @description Is used to show an id inside the note.*/
     @Input() inputId:number;
-    /** @description Is used to give the header a text.*/
     @Input() inputHeaderText:string;
-    /** @description Is used to fill the note with text.*/
     @Input() inputNoteText:string;
 
     constructor()
